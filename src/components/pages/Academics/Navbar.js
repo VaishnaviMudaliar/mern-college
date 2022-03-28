7b0acaa6dc5a09368fff479e8a1d799fcf8ee5c3
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2022-03-28 20:06:46 +0530

    close icon added

--- src/components/pages/Academics/Navbar.js
+++ src/components/pages/Academics/Navbar.js
@@ -1,28 +1,25 @@
-import { Component } from "react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <div className="navbar">
-          <Link to="#" className="menu-bars">
-            <FaIcons.FaBars />
-          </Link>
-        </div>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items">
-            <li classname="navbar-toggle"></li>
-            <Link to="#" className="menu-bars">
-              <FaIcons.AiOutlineClose />
-            </Link>
-          </ul>
-        </nav>
+export default function Navbar() {
+  const [sidebar, setSidebar] = useState(false);
+  return (
+    <div>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FaIcons.FaBars />
+        </Link>
       </div>
-    );
-  }
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items">
+          <li classname="navbar-toggle"></li>
+          <Link to="#" className="menu-bars">
+            <AiIcons.AiOutlineClose />
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
-
-export default Navbar;
