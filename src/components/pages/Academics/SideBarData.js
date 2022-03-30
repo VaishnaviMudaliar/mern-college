b326a9117021ee11486ba3d26ee57fa54bd75382
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2022-03-30 13:17:50 +0530

    creating the sidebardata

--- src/components/pages/Academics/SideBarData.js
+++ src/components/pages/Academics/SideBarData.js
@@ -1,1 +1,12 @@
 import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SideBarData = [
+  {
+    Home: "title",
+    path: "/",
+    icon: <AiIcons.AiFillHome />
+  }
+];
