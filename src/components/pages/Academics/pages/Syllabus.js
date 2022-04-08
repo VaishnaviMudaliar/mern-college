3b4d33fc45c08cc17129dcb4a80c18dbba1e4bed
Vaishnavi Mudaliar <vaishnavim1311@gmail.com>
2022-04-08 13:28:41 +0530

    syllabus page done

--- src/components/pages/Academics/pages/Syllabus.js
+++ src/components/pages/Academics/pages/Syllabus.js
@@ -1,27 +1,33 @@
 import React from "react";
+import "./syllabus.css";
+import computer from "../../assets/computer.jpg";
+import mechanical from "../../assets/mechanical.jpg";
+import civil from "../../assets/civil.jpg";
 
 export default function Syllabus() {
   return (
     <div>
+      <h1>Syllabus</h1>
+      <h1 className="heading"> Please Choose The Department</h1>
       <div className="row">
         <div className="campus-col">
-          <img src="" alt="" />
+          <img src={computer} alt="" />
           <div className="layer">
-            <h3>LONDON</h3>
+            <h3>Computer Engineering</h3>
           </div>
         </div>
 
         <div className="campus-col">
-          <img src="images/newyork.png" alt="" />
+          <img src={mechanical} alt="" />
           <div class="layer">
-            <h3>NEW YORK</h3>
+            <h3>Mechanical Engineering</h3>
           </div>
         </div>
 
         <div class="campus-col">
-          <img src="images/washington.png" alt="" />
+          <img src={civil} alt="" />
           <div className="layer">
-            <h3>WASHINGTON</h3>
+            <h3>Civil Engineering </h3>
           </div>
         </div>
       </div>
